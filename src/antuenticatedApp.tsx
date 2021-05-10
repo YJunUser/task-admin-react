020425c37598a4bab379c98b636a4f0f73f0ce45
YJunUser <1239923786@qq.com>
2021-05-10 14:45:08 +0800

    style: emotion封装flex，用row布局页面

--- src/antuenticatedApp.tsx
+++ src/antuenticatedApp.tsx
@@ -1,13 +1,41 @@
 import { ProjectListScreen } from "screens/project-list";
 import React from "react";
 import { useAuth } from "./context/auth-context";
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <ProjectListScreen></ProjectListScreen>
-      <button onClick={() => logout()}>登出</button>
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>logo</h3>
+          <h3>我的</h3>
+          <h3>项目</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={() => logout()}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen></ProjectListScreen>
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+// 这里value没有引号
+const Header = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+`;
