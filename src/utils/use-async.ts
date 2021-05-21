2ab856342185396036b72d4dd788a42bb0d2f1f7
YJunUser <1239923786@qq.com>
2021-05-21 19:15:24 +0800

    fix: 用usereducer重写use-async

--- src/utils/use-async.ts
+++ src/utils/use-async.ts
@@ -1,7 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useReducer } from "react";
 import { AxiosPromise, AxiosResponse } from "axios";
 import { useMountedRef } from "./index";
+import { Project } from "./type";
 
+// useState适合于单个状态， useReducer适合于多个且复杂的状态
 interface State<D> {
   error: Error | null; // 返回的错误
   data: D | null; // 返回的数据
@@ -18,6 +20,15 @@ const defualtConcfig = {
   throwError: false,
 };
 
+// 函数柯里化 arguments变量还记得吗
+const useSafeDispatch = <T>(dispatch: (...args: T[]) => void) => {
+  const mountedRef = useMountedRef();
+  return useCallback(
+    (...args: T[]) => (mountedRef.current ? dispatch(...args) : void 0),
+    [dispatch, mountedRef]
+  );
+};
+
 export const useAsync = <D>(
   intial?: State<D>,
   intialConfig?: typeof defualtConcfig
@@ -26,43 +37,42 @@ export const useAsync = <D>(
   // useState或者setState参数为函数时，不会保存函数，而是在初次渲染时执行，返回一个state的值，详情见reactapi惰性初始化
   // 所以如果我们要保存函数，就在要保存函数的外面再加一层函数 React.useState(() => () => {return '这是我们要保存的函数'}),
   // 或者用useRef const callbackRef = useRef(() => alert('init')) const callback = callbackRef.current 注意对useRef修改不会触发组件重新渲染
-  const [state, setState] = useState<State<D>>({
-    ...intial,
-    ...defaultIntialState,
-  });
+  // const [state, setState] = useState<State<D>>({
+  //   ...intial,
+  //   ...defaultIntialState,
+  // });
+  const [state, dispatch] = useReducer(
+    (state: State<D>, action: Partial<State<D>>) => ({ ...state, ...action }),
+    {
+      ...intial,
+      ...defaultIntialState,
+    }
+  );
+
   const [retry, setRetry] = useState(() => () => {});
 
-  const mountedRef = useMountedRef();
+  const safeDispatch = useSafeDispatch(dispatch);
 
-  // const setError = (error: Error) => {
-  //   setState({
-  //     error,
-  //     data: null,
-  //     stat: "error",
-  //   });
-  // };
-
-  // const setData = (data: D) => {
-  //   setState({
-  //     error: null,
-  //     data,
-  //     stat: "success",
-  //   });
-  // };
-  const setError = useCallback((error: Error) => {
-    setState({
-      error,
-      data: null,
-      stat: "error",
-    });
-  }, []);
-  const setData = useCallback((data: D) => {
-    setState({
-      error: null,
-      data,
-      stat: "success",
-    });
-  }, []);
+  const setError = useCallback(
+    (error: Error) => {
+      safeDispatch({
+        error,
+        data: null,
+        stat: "error",
+      });
+    },
+    [safeDispatch]
+  );
+  const setData = useCallback(
+    (data: D) => {
+      safeDispatch({
+        error: null,
+        data,
+        stat: "success",
+      });
+    },
+    [safeDispatch]
+  );
 
   // AxiosPromise 是axios请求第一次返回的值，包含6个字段，其中data字段是我们需要的，泛型D表示data类型 AxiosPromise extends Promise<AxiosResponse>
   // 需要非基本类型做依赖时， 用useCallback或者useMemo
@@ -86,21 +96,23 @@ export const useAsync = <D>(
       //   ...state,
       //   stat: "loading",
       // });
-      setState((preState) => ({ ...preState, stat: "loading" }));
+      safeDispatch({ stat: "loading" });
 
       return promise
         .then((res: AxiosResponse<D>) => {
           if (res) {
             // 防止在已卸载组件上赋值这种情况, 即将已请求到的数据赋值给已卸载的数据，这样会报错
-            if (mountedRef.current) {
-              setData(res.data);
-            }
+            // if (mountedRef.current) {
+            //   setData(res.data);
+            // }
+            setData(res.data);
           }
           return res.data;
         })
         .catch((error) => {
           if (error) {
-            if (mountedRef.current) setError(error);
+            // if (mountedRef.current) setError(error);
+            setError(error);
           }
 
           // catch会消化异常，如果不主动抛出，外面是接受不到的
@@ -111,7 +123,7 @@ export const useAsync = <D>(
           }
         });
     },
-    [config.throwError, mountedRef, setData, setError, setState]
+    [config.throwError, setData, setError, safeDispatch]
     // 依赖中的函数必须也用useCallback去定义, 依赖改变了就重新执行useCallback， 你在run里setState了，state就会改变， 所以如果要在回调里使用state，请使用函数state方法
   );
 
--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,42 +1,47 @@
-import React, { useState } from "react";
+import React, {
+  Reducer,
+  ReducerWithoutAction,
+  useReducer,
+  useState,
+} from "react";
 import { useCallback } from "react";
 
-export const useUndo = <T>(initialValue: T) => {
-  const [state, setState] = useState<{
-    past: T[];
-    present: T;
-    future: T[];
-  }>({
-    past: [],
-    present: initialValue,
-    future: [],
-  });
+// 在TypeScript中，typeof操作符可以用来获取一个变量或对象的类型。
+const UNDO = "UNDO";
+const REDO = "REDO";
+const SET = "SET";
+const RESET = "RESEET";
 
-  const canUndo = state.past.length !== 0;
-  const canRedo = state.future.length !== 0;
+type State<T> = {
+  past: T[];
+  present: T;
+  future: T[];
+};
 
-  const undo = useCallback(
-    () =>
-      setState((currentState) => {
-        const { past, present, future } = currentState;
-        if (past.length === 0) return currentState;
+type Action<T> = {
+  newPresent?: T;
+  type: typeof UNDO | typeof REDO | typeof SET | typeof RESET;
+};
+// 用useReducer来管理
+// useReducer中state是最新的
+const reducer = <T>(state: State<T>, action: Action<T>) => {
+  switch (action.type) {
+    case UNDO: {
+      const { past, present, future } = state;
+      if (past.length === 0) return state;
 
-        const previous = past[past.length - 1];
-        const newPast = past.slice(0, past.length - 1);
+      const previous = past[past.length - 1];
+      const newPast = past.slice(0, past.length - 1);
 
-        return {
-          past: newPast,
-          present: previous,
-          future: [present, ...future],
-        };
-      }),
-    []
-  );
-
-  const redo = useCallback(() => {
-    setState((currentState) => {
-      const { past, present, future } = currentState;
-      if (future.length === 0) return currentState;
+      return {
+        past: newPast,
+        present: previous,
+        future: [present, ...future],
+      };
+    }
+    case REDO: {
+      const { past, present, future } = state;
+      if (future.length === 0) return state;
 
       const next = future[0];
       const newFuture = future.slice(1);
@@ -47,30 +52,129 @@ export const useUndo = <T>(initialValue: T) => {
         present: next,
         future: newFuture,
       };
-    });
-  }, []);
-
-  const set = useCallback((value: T) => {
-    setState((currentState) => {
-      const { past, present, future } = currentState;
-      if (value === present) return currentState;
+    }
+    case SET: {
+      const { past, present, future } = state;
+      if (action.newPresent === present) return state;
       return {
         past: [...past, present],
-        present: value,
+        present: action.newPresent,
         future: [],
       };
-    });
-  }, []);
-
-  const reset = useCallback((value: T) => {
-    setState(() => {
+    }
+    case RESET: {
       return {
         past: [],
-        present: value,
+        present: action.newPresent,
         future: [],
       };
-    });
+    }
+    default:
+      throw new Error("type is not exist");
+  }
+};
+
+export const useUndo = <T>(intialValue: T) => {
+  // 用泛型或者类型断言都行
+  const [state, dispatch] = useReducer(reducer, {
+    past: [],
+    present: intialValue,
+    future: [],
+  } as State<T>);
+
+  const canUndo = state.past.length !== 0;
+  const canRedo = state.future.length !== 0;
+
+  const undo = useCallback(() => {
+    // dispatch就是调用reducer函数，传入action参数
+    dispatch({ type: UNDO });
+  }, []);
+
+  const redo = useCallback(() => {
+    dispatch({ type: REDO });
+  }, []);
+
+  const set = useCallback((newPresent: T) => {
+    dispatch({ type: SET, newPresent });
+  }, []);
+
+  const reset = useCallback((newPresent: T) => {
+    dispatch({ type: RESET, newPresent });
   }, []);
 
   return [state, { set, reset, undo, redo, canUndo, canRedo }];
 };
+// export const useUndo = <T>(initialValue: T) => {
+//   // 对于复杂的state操作逻辑，嵌套的state的对象，推荐使用useReducer。
+//   const [state, setState] = useState<{
+//     past: T[];
+//     present: T;
+//     future: T[];
+//   }>({
+//     past: [],
+//     present: initialValue,
+//     future: [],
+//   });
+
+//   const canUndo = state.past.length !== 0;
+//   const canRedo = state.future.length !== 0;
+
+//   const undo = useCallback(
+//     () =>
+//       setState((currentState) => {
+//         const { past, present, future } = currentState;
+//         if (past.length === 0) return currentState;
+
+//         const previous = past[past.length - 1];
+//         const newPast = past.slice(0, past.length - 1);
+
+//         return {
+//           past: newPast,
+//           present: previous,
+//           future: [present, ...future],
+//         };
+//       }),
+//     []
+//   );
+
+//   const redo = useCallback(() => {
+//     setState((currentState) => {
+//       const { past, present, future } = currentState;
+//       if (future.length === 0) return currentState;
+
+//       const next = future[0];
+//       const newFuture = future.slice(1);
+//       const newPast = [...past, present];
+
+//       return {
+//         past: newPast,
+//         present: next,
+//         future: newFuture,
+//       };
+//     });
+//   }, []);
+
+//   const set = useCallback((value: T) => {
+//     setState((currentState) => {
+//       const { past, present, future } = currentState;
+//       if (value === present) return currentState;
+//       return {
+//         past: [...past, present],
+//         present: value,
+//         future: [],
+//       };
+//     });
+//   }, []);
+
+//   const reset = useCallback((value: T) => {
+//     setState(() => {
+//       return {
+//         past: [],
+//         present: value,
+//         future: [],
+//       };
+//     });
+//   }, []);
+
+//   return [state, { set, reset, undo, redo, canUndo, canRedo }];
+// };
